98789a00335ae2367df88b4379784aae16f6844d
Kyle Robinson Young <kyle@dontkry.com>
2016-04-04 19:15:22 -0700

    Add skipIfExist argument

--- tasks/contrib-ci.js
+++ tasks/contrib-ci.js
@@ -10,13 +10,18 @@
 
 module.exports = function(grunt) {
   var path = require('path');
-  grunt.registerTask('contrib-ci', 'Normalizes appveyor and travis ci configs.', function() {
+  grunt.registerTask('contrib-ci', 'Normalizes appveyor and travis ci configs.', function(skipIfExists) {
+    skipIfExists = skipIfExists === true;
     var travis = grunt.file.read(path.join(__dirname, '..', '.travis.yml'));
     var appveyor = grunt.file.read(path.join(__dirname, '..', 'appveyor.yml'));
     var taskTravis = path.join(process.cwd(), '.travis.yml');
+    if (!skipIfExists || !grunt.file.exists(taskTravis)) {
+      grunt.file.write(taskTravis, travis);
+    }
     var taskAppveyor = path.join(process.cwd(), 'appveyor.yml');
-    grunt.file.write(taskTravis, travis);
-    grunt.file.write(taskAppveyor, appveyor);
+    if (!skipIfExists || !grunt.file.exists(taskAppveyor)) {
+      grunt.file.write(taskAppveyor, appveyor);
+    }
     grunt.log.ok('Normalized .travis.yml and appveyor.yml for grunt-contrib.');
   });
 };
