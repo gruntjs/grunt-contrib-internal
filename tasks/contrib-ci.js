03399dc98fd06af86ea38bff097c2d308f44cace
XhmikosR <xhmikosr@gmail.com>
2016-04-10 02:56:11 +0300

    Minor tweaks for contrib-ci.js.

--- tasks/contrib-ci.js
+++ tasks/contrib-ci.js
@@ -9,19 +9,25 @@
 'use strict';
 
 module.exports = function(grunt) {
-  var path = require('path');
   grunt.registerTask('contrib-ci', 'Normalizes AppVeyor and Travis CI configs.', function(skipIfExists) {
-    skipIfExists = skipIfExists === "skipIfExists";
+    skipIfExists = skipIfExists === 'skipIfExists';
+
+    var path = require('path');
     var travis = grunt.file.read(path.join(__dirname, '..', '.travis.yml'));
     var appveyor = grunt.file.read(path.join(__dirname, '..', 'appveyor.yml'));
     var taskTravis = path.join(process.cwd(), '.travis.yml');
-    if (!skipIfExists || !grunt.file.exists(taskTravis)) {
-      grunt.file.write(taskTravis, travis);
-    }
     var taskAppveyor = path.join(process.cwd(), 'appveyor.yml');
-    if (!skipIfExists || !grunt.file.exists(taskAppveyor)) {
-      grunt.file.write(taskAppveyor, appveyor);
+
+    if (!skipIfExists) {
+      if (!grunt.file.exists(taskTravis)) {
+        grunt.file.write(taskTravis, travis);
+      }
+
+      if (!grunt.file.exists(taskAppveyor)) {
+        grunt.file.write(taskAppveyor, appveyor);
+      }
+      grunt.log.ok('Normalized .travis.yml and appveyor.yml for grunt-contrib.');
     }
-    grunt.log.ok('Normalized .travis.yml and appveyor.yml for grunt-contrib.');
+
   });
 };
